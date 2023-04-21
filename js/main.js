c2dcb0b8c1807830248562ded72b3192394a5f46
Fernanda Santos <santo4fer@gmail.com>
2023-04-20 21:53:15 -0300

    adicionando dinamica js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,53 @@
-const robotron = document.querySelector('#robotron')
 
-robotron.addEventListener("click", dizOi)
+const controle = document.querySelectorAll("[data-controle]")
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
 
-function dizOi() {
-    console.log("oi")
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos": {
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas": {
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes": {
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+
+controle.forEach((elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+
+    })
+})
+
+
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector('[data-contador]')
+
+    if (operacao === "-") {
+        peca.value = parseInt(peca.value) - 1
+    } else {
+        peca.value = parseInt(peca.value) + 1
+    }
 }
