1b729ff39204d0de5f1eae420205a321cede8ad4
Fernanda Santos <santo4fer@gmail.com>
2023-04-21 18:18:47 -0300

    mudando estatisticas

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 
 const controle = document.querySelectorAll("[data-controle]")
+const estatisticas = document.querySelectorAll("[data-estatistica]")
+console.log(estatisticas)
 const pecas = {
     "bracos": {
         "forca": 29,
@@ -37,10 +39,10 @@ const pecas = {
 controle.forEach((elemento) => {
     elemento.addEventListener("click", (evento) => {
         manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+        atualizaEstatisticas(evento.target.dataset.peca)
 
     })
 })
-
 
 function manipulaDados(operacao, controle) {
     const peca = controle.querySelector('[data-contador]')
@@ -50,4 +52,12 @@ function manipulaDados(operacao, controle) {
     } else {
         peca.value = parseInt(peca.value) + 1
     }
+}
+
+function atualizaEstatisticas(peca) {
+
+    estatisticas.forEach((elemento) => {
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+
+    })
 }
